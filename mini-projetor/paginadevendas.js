8ee7c7408dd3d1e3df6c79794f04859a545f481e
virturios <135859497+virturios@users.noreply.github.com>
2023-06-16 00:47:31 -0300

    Update paginadevendas.js

--- mini-projetor/paginadevendas.js
+++ mini-projetor/paginadevendas.js
@@ -1,12 +1,9 @@
 
 
 window.addEventListener('load', function () {
-    $(".headerdesconto").fadeTo(2000,1);
+
 
     
-    x = $("#vimeoplayer").attr("data-id");
-    $("#vimeoplayer").attr("src","https://player.vimeo.com/video/"+x);
-
     var d = new Date();
     
     $( ".data-coment" ).each(function( index ) {
@@ -64,32 +61,5 @@ window.addEventListener('load', function () {
 
 
 
-    jQuery(document).ready(function (e) {
-      var t = new Date();
-        g = t.setDate(t.getDate());
-        o = t.setDate(t.getDate() - 15);
-        n = t.setDate(t.getDate() + 10 + 15);
-        r = getMesExtenso(t.getMonth());
-        var dataarrumada = convertDate(new Date()) + " de " + r;
-        if (convertDate(o) > convertDate(n)) var a = "<strong>" + convertDate(n) + "</strong> e <strong>" + convertDate(o) + " de " + r + "</strong>";
-        else {
-            if (null == (s = getMesExtenso(t.getMonth() + 1))) var s = getMesExtenso(t.getMonth() - 11);
-            a = "<strong>" + convertDate(n) + " de " + r + "</strong> e <strong>" + convertDate(o) + " de " + s + "</strong>";
-        }
-      e.getJSON("https://wtfismyip.com/json", function (t) {
-          var o = (t = t.YourFuckingLocation).replace(", Brazil", "");
-          var posicao = o.indexOf(",");
-          var tratado = o.substring(0, posicao);
-          e(".custom-address").html("<font color='#00A650'>via <b>Correios</b></font> <font color='#00A650'> para </font> <strong><font color='#00A650'>" + o + " e Região</font></strong>"),
-        e(".shipping-estimated").html("<font color='#00A650'>Chegará grátis entre " + a + ".</font>"),
-        e(".shipping-estimated2").html("<font color='#00A650'>Chegará <b>grátis</b> entre " + a + ".</font>"),
-
-        e(".shipping-preview-loading").addClass("d-none"),
-          e(".cidade").html(tratado),
-              e(".cidaderegiao1").html(o),
-              e(".cidaderegiao").html(o + " e região");  
-             
-      });
-  });
-
+   
 })
